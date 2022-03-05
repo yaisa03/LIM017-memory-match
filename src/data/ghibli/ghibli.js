e7a754b1f5e0bef5211303b192736e1ee7c3f056
yaisa03 <yaisamichellea@gmail.com>
2022-03-05 00:54:48 -0500

    primera historia y vista de juego pokemon

---
+++ src/data/ghibli/ghibli.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'Ghibli',
+  items: [
+    { id: 'chihiro', image: 'https://www.serebii.net/pokemongo/pokemon/001.png', bgColor: '#339933' },
+    { id: 'totoro', image: 'https://www.serebii.net/pokemongo/pokemon/002.png', bgColor: '#339933' },
+    { id: 'howl', image: 'https://www.serebii.net/pokemongo/pokemon/003.png', bgColor: '#339933' },
+    { id: 'charmander', image: 'https://www.serebii.net/pokemongo/pokemon/004.png', bgColor: '#F05032' },
+    { id: 'charmeleon', image: 'https://www.serebii.net/pokemongo/pokemon/005.png', bgColor: '#F05032' },
+    { id: 'charizard', image: 'https://www.serebii.net/pokemongo/pokemon/006.png', bgColor: '#F05032' },
+    { id: 'squirtle', image: 'https://www.serebii.net/pokemongo/pokemon/007.png', bgColor: '#1572B6' },
+    { id: 'wartortle', image: 'https://www.serebii.net/pokemongo/pokemon/008.png', bgColor: '#1572B6' },
+    { id: 'blastoise', image: 'https://www.serebii.net/pokemongo/pokemon/009.png', bgColor: '#1572B6' },
+  ],
+};
--- src/main.js
+++ src/main.js
@@ -1,3 +1,46 @@
-import App from './components/App.js';
+//import webdeb from './data/webdeb/webdeb.js';
+import pokemon from './data/pokemon/pokemon.js';
+//import ghibli from './data/ghibli/ghibli.js';
 
-document.getElementById('root').appendChild(App());
+/*data importada
+const dataWbDeb = webdeb.items;*/
+const dataPokemon = pokemon.items;
+//const dataGhibli = pokemon.items;
+
+/*vista de los juegos por tematica*/
+const memoryGameOptions = document.getElementById("memoryGameOptions");
+
+/*funcion que crea las cartas*/
+function showCards(data, gameImage) {
+    let cardsDiv = "";
+    data.forEach(e => {
+        cardsDiv += `<div id="${e.id} class="memorycard">
+                        <img class="front-card" src="${e.image}">
+                        <img class="back-card" src="${gameImage}">
+                    </div>
+                    <div id="${e.id} class="memorycard">
+                        <img class="front-card" src="${e.image}">
+                        <img class="back-card" src="${gameImage}">
+                    </div>`
+        gamePokemon.innerHTML = cardsDiv;
+    });
+    return cardsDiv;
+}
+
+//Juego de tematica POKEMON
+//contendor de las cartas
+let gamePokemon = document.getElementById("gamePokemon");
+//imagen de la carta de adelante para el juego
+const imageGamePokemon = "images/gamePokemon.png";
+//para seleccionar el juego
+const choosePokemon = document.getElementById("choosePokemon"); //contenedor
+choosePokemon.addEventListener('click', () => { //evento que inicia
+    gamePokemon.style.display = "";
+    memoryGameOptions.style.display = "none";
+    showCards(dataPokemon, imageGamePokemon);
+    //funcion que chocolatea cartas, el resultado va a gamePokemon
+});
+//const gameStudioGhibli = document.getElementById("gameStudioGhibli");
+//const gameWebDeb = document.getElementById("gameWebDeb");
+
+
